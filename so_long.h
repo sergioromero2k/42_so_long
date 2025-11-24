285955d4f740d83a6d291c4b946cf46fa7a7f6cb
Sergio Alejandro <sergioromero2k@gmail.com>
2025-11-24 22:04:48 +0100

    Empezando so_long

---
+++ so_long.h
@@ -0,0 +1,14 @@
+#ifndef SOLONG
+# define SOLONG
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <stdarg.h>
+#include <stdbool.h>
+#include <unistd.h>
+#include <fcntl.h>
+
+#endif
