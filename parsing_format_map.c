d0cf3fc72b23298e54ece544b945b8ad3d72ae3c
Sergio Alejandro <sergioromero2k@gmail.com>
2025-11-26 20:18:55 +0100

    Parsing format map

---
+++ parsing_format_map.c
@@ -0,0 +1,23 @@
+#include "so_long.h"
+
+int	main(int argc, char *argv[])
+{
+  char *ptr; 
+  if(argc!=2)
+    return printf("Error p"), EXIT_FAILURE;
+
+  ptr=&(*argv[1]); 
+  while(*ptr!='.')
+    ptr++;
+ 
+  ptr++;
+  if(*ptr!='b')
+    return (printf("Error b"), EXIT_FAILURE);
+  ptr++;
+  if(*ptr!='e') 
+    return (printf("Error e"), EXIT_FAILURE) ;
+  ptr++;
+  if(*ptr!='r')
+    return (printf("Error r"), EXIT_FAILURE);
+}
+
--- so_long.h
+++ so_long.h
@@ -1,14 +1,12 @@
 #ifndef SOLONG
 # define SOLONG
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <stdlib.h>
-#include <string.h>
-#include <limits.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <math.math.h>
+# include <fcntl.h>
+# include <limits.h>
+# include <stdarg.h>
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 #endif
