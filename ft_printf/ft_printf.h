7422ba94e025f490cb2d6c7de5976223e45260c8
Sergio Alejandro <sergioromero2k@gmail.com>
2025-12-09 15:49:45 +0100

    Clasificacion en carpetas

--- ft_printf/ft_printf.h
+++ ft_printf/ft_printf.h
@@ -1,39 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: sergio-alejandro <sergio-alejandro@stud    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/10/18 21:05:20 by sergio-alej       #+#    #+#             */
-/*   Updated: 2025/11/22 20:42:29 by sergio-alej      ###   ########.fr       */
+/*   Created: 2025/11/22 20:49:50 by sergio-alej       #+#    #+#             */
+/*   Updated: 2025/11/22 20:56:35 by sergio-alej      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
 
-# ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 42
-# endif
-
-# include <fcntl.h>
 # include <limits.h>
-# include <stdbool.h>
+# include <stdarg.h>
 # include <stdint.h>
 # include <stdio.h>
 # include <stdlib.h>
 # include <string.h>
 # include <unistd.h>
 
-size_t	ft_strlen(char *s);
-char	*ft_strchr(char *s, int c);
-char	*ft_strdup(char *s);
-char	*ft_strjoin(char *s1, char *s2);
-size_t	ft_strlen_until_jump_line(char *s);
-char	*read_one_line(char *line);
-char	*read_file_descriptor(int fd, char *text);
-char	*verify_assigned(char *text);
-char	*get_next_line(int fd);
-
+void	ft_putchar(char c, int *count);
+void	ft_putstr(char *s, int *count);
+void	ft_putptr(void *ptr, int *count);
+void	ft_putnbr_base(long long int n, int base, char *formato, int *count);
+void	ft_putnbr_base_unsigned(unsigned long long int n, int base,
+			char *formato, int *count);
+int		ft_strlen(const char *s);
+int		ft_printf(char const *format, ...);
 #endif
